b994379d5e6d0dc6af6cd8f047f6e1542d2ca323
alexander.biryukov <alexander.biryukov@p2p.org>
2025-01-15 16:48:38 +0400

    fix P2pMorphoProxyFactory deposit

--- src/adapters/P2pMorphoProxyFactory.sol
+++ src/adapters/P2pMorphoProxyFactory.sol
@@ -3,8 +3,11 @@
 
 pragma solidity 0.8.27;
 
+import "../@permit2/interfaces/IAllowanceTransfer.sol";
 import "../p2pLendingProxyFactory/P2pLendingProxyFactory.sol";
 
+error P2pMorphoProxyFactory__IncorrectLengthOf_dataForMulticall();
+error P2pMorphoProxyFactory__InvalidSlice();
 error P2pMorphoProxyFactory__approve2_amount_ne_permitSingleForP2pLendingProxy_amount();
 error P2pMorphoProxyFactory__transferFrom2_amount_ne_permitSingleForP2pLendingProxy_amount();
 error P2pMorphoProxyFactory__erc4626Deposit_assets_ne_permitSingleForP2pLendingProxy_amount();
@@ -13,6 +16,8 @@ error P2pMorphoProxyFactory__transferFrom2_asset_ne_permitSingleForP2pLendingPro
 error P2pMorphoProxyFactory__erc4626Deposit_receiver_ne_proxy();
 
 contract P2pMorphoProxyFactory is P2pLendingProxyFactory {
+
+    uint256 private constant SELECTOR_LENGTH = 4;
 
     /// @notice Constructor for P2pMorphoProxyFactory
     /// @param _morphoBundler The morpho bundler address
@@ -41,27 +46,29 @@ contract P2pMorphoProxyFactory is P2pLendingProxyFactory {
     override
     returns (address p2pLendingProxyAddress) {
         // morpho multicall
-        (, bytes[] memory dataForMulticall) = abi.decode(
-            _lendingProtocolCalldata,
-            (bytes4, bytes[])
+        bytes[] memory dataForMulticall = abi.decode(_lendingProtocolCalldata[SELECTOR_LENGTH:], (bytes[]));
+
+        require(
+            dataForMulticall.length == 3,
+            P2pMorphoProxyFactory__IncorrectLengthOf_dataForMulticall()
         );
 
         // morpho approve2
-        (, IAllowanceTransfer.PermitSingle memory permitSingle,,) = abi.decode(
-            dataForMulticall[0],
-            (bytes4, IAllowanceTransfer.PermitSingle, bytes, bool)
+        (IAllowanceTransfer.PermitSingle memory permitSingle,,) = abi.decode(
+            _slice(dataForMulticall[0], SELECTOR_LENGTH, dataForMulticall[0].length - SELECTOR_LENGTH),
+            (IAllowanceTransfer.PermitSingle, bytes, bool)
         );
 
         // morpho transferFrom2
-        (, address asset, uint256 amount) = abi.decode(
-            dataForMulticall[1],
-            (bytes4, address, uint256)
+        (address asset, uint256 amount) = abi.decode(
+            _slice(dataForMulticall[1], SELECTOR_LENGTH, dataForMulticall[1].length - SELECTOR_LENGTH),
+            (address, uint256)
         );
 
         // morpho erc4626Deposit
-        (,, uint256 assets,, address receiver) = abi.decode(
-            dataForMulticall[2],
-            (bytes4, address, uint256, uint256, address)
+        (,uint256 assets,, address receiver) = abi.decode(
+            _slice(dataForMulticall[2], SELECTOR_LENGTH, dataForMulticall[2].length - SELECTOR_LENGTH),
+            (address, uint256, uint256, address)
         );
 
         require(
@@ -106,5 +113,40 @@ contract P2pMorphoProxyFactory is P2pLendingProxyFactory {
             _p2pSignerSigDeadline,
             _p2pSignerSignature
         );
+    }
+
+    // Helper function to slice bytes
+    function _slice(bytes memory data, uint256 start, uint256 length) internal pure returns (bytes memory) {
+        require(
+            data.length >= (start + length),
+            P2pMorphoProxyFactory__InvalidSlice()
+        );
+
+        bytes memory tempBytes;
+
+        assembly {
+            switch iszero(length)
+            case 0 {
+            // Allocate memory for the sliced bytes
+                tempBytes := mload(0x40)
+            // Set the length
+                mstore(tempBytes, length)
+            // Copy the data
+                let src := add(data, add(0x20, start))
+                let dest := add(tempBytes, 0x20)
+                for { let i := 0 } lt(i, length) { i := add(i, 0x20) } {
+                    mstore(add(dest, i), mload(add(src, i)))
+                }
+            // Update the free memory pointer
+                mstore(0x40, add(dest, length))
+            }
+            default {
+                tempBytes := mload(0x40)
+                mstore(tempBytes, 0)
+                mstore(0x40, add(tempBytes, 0x20))
+            }
+        }
+
+        return tempBytes;
     }
 }
