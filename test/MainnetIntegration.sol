5064708216e921757fbad0b107cb4358c00482b6
alexander.biryukov <alexander.biryukov@p2p.org>
2025-01-16 10:36:41 +0400

    2025

--- test/MainnetIntegration.sol
+++ test/MainnetIntegration.sol
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 P2P Validator <info@p2p.org>
+// SPDX-FileCopyrightText: 2025 P2P Validator <info@p2p.org>
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.27;
