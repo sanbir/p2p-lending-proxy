a8ea946eaaa02866e61b0efddbfcdb319fb9a8a7
alexander.biryukov <alexander.biryukov@p2p.org>
2025-04-02 14:12:48 +0400

    OptimismIntegration cleanup

--- src/adapters/superform/p2pSuperformProxy/P2pSuperformProxy.sol
+++ src/adapters/superform/p2pSuperformProxy/P2pSuperformProxy.sol
@@ -152,22 +152,22 @@ contract P2pSuperformProxy is P2pYieldProxy, IP2pSuperformProxy {
     }
 
     function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data
-    ) external returns (bytes4) {
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
         return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
     }
 
     function onERC1155BatchReceived(
-        address operator,
-        address from,
-        uint256[] calldata ids,
-        uint256[] calldata values,
-        bytes calldata data
-    ) external returns (bytes4) {
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external pure returns (bytes4) {
         return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
     }
 
