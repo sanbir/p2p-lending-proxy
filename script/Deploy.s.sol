7f8dde01907443d7ba3934bbbd7c5745e8e74309
alexander.biryukov <alexander.biryukov@p2p.org>
2025-06-18 16:35:48 +0400

    deploy Resolv

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,31 +4,31 @@
 pragma solidity 0.8.27;
 
 import "../lib/forge-std/src/Vm.sol";
-import "../src/adapters/ethena/p2pEthenaProxyFactory/P2pEthenaProxyFactory.sol";
+import "../src/adapters/resolv/p2pResolvProxyFactory/P2pEthenaProxyFactory.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract Deploy is Script {
-    address constant USDe = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
-    address constant sUSDe = 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497;
+    address constant USR = 0x66a1E37c9b0eAddca17d3662D6c05F4DECf3e110;
+    address constant stUSR = 0x6c8984bc7DBBeDAf4F6b2FD766f16eBB7d10AAb4;
     address constant P2pTreasury = 0xfeef177E6168F9b7fd59e6C5b6c2d87FF398c6FD;
 
     function run()
         external
-        returns (P2pEthenaProxyFactory factory, P2pEthenaProxy proxy)
+        returns (P2pResolvProxyFactory factory, P2pResolvProxy proxy)
     {
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         Vm.Wallet memory wallet = vm.createWallet(deployerKey);
 
         vm.startBroadcast(deployerKey);
-            factory = new P2pEthenaProxyFactory(
+            factory = new P2pResolvProxyFactory(
                 wallet.addr,
                 P2pTreasury,
-                sUSDe,
-                USDe
+                stUSR,
+                USR
             );
         vm.stopBroadcast();
 
-        proxy = P2pEthenaProxy(factory.getReferenceP2pYieldProxy());
+        proxy = P2pResolvProxy(factory.getReferenceP2pYieldProxy());
 
         return (factory, proxy);
     }
